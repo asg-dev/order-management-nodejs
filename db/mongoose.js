e08184c3064c2124d2daf6889f458bcb09679787
Vaishnav <vaishnavthirumoorthy@gmail.com>
2021-03-26 16:43:51 +0530

    modified mongo file

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const connectDB = async () => {
     try{
-        const conn =  await mongoose.connect('mongodb://127.0.0.1:27017/test', {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true})
+        const conn =  await mongoose.connect('mongodb://127.0.0.1:27017/<db_name>', {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true})
         console.log(`Mongodb connected`)
     }
     catch(error)
